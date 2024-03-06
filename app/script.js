68cdd6bb6166016bfcf94cdf9f5ed45198fb37a8
victorluansilva <victorluansilva@gmail.com>
2024-03-06 11:14:06 -0300

    Adicionando estrutura de cadastro de comentários

--- app/script.js
+++ app/script.js
@@ -1,8 +1,41 @@
-let data = [{}]
+let data = []
 
-const submitComment = (event) =>{
+const submitComment = (event) => {
     event.preventDefault();
 
-    console.log(event)
+    const author = inputAuthor.value;
+    const comment = inputComment.value;
+
+    data.push({ author: author, comment: comment })
+    console.log(data)
+
+    loadComment()
+}
+
+const formComentario = document.getElementById('formComment')
+formComentario.addEventListener("submit", submitComment)
+
+const loadComment = () => {
+    // Dados carregados da API
+    if (data) {
+        //displayComment();
+    }
+}
+
+const displayComment = () => {
+    const body = document.getElementsByTagName('body')[0];
+
+    data.forEach(elemento => {
+        const divDisplay = document.createElement('div');
+        divDisplay.className = 'comentarios'
+        divDisplay.innerHTML = `
+        <strong>
+        <span class="badge text-bg-secondary">${elemento.author}</span>
+        </strong>
+        <p>${elemento.comment}</p>
+        `
+        body.appendChild(divDisplay);
+    })
+
 
 }
