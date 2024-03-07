fa58bfdb92ecf1331194dd73130fd451e2f4aedd
victorluansilva <victorluansilva@gmail.com>
2024-03-07 09:01:07 -0300

    Estilização do feed de comentários

--- app/script.js
+++ app/script.js
@@ -18,24 +18,33 @@ formComentario.addEventListener("submit", submitComment)
 const loadComment = () => {
     // Dados carregados da API
     if (data) {
-        //displayComment();
+        displayComment();
     }
 }
 
 const displayComment = () => {
-    const body = document.getElementsByTagName('body')[0];
-
-    data.forEach(elemento => {
+    const divFeed = document.getElementById('comment-feed');
+    divFeed.innerHTML = ``
+    data.forEach(item => {
         const divDisplay = document.createElement('div');
         divDisplay.className = 'comentarios'
         divDisplay.innerHTML = `
-        <strong>
-        <span class="badge text-bg-secondary">${elemento.author}</span>
-        </strong>
-        <p>${elemento.comment}</p>
+        
+        <div class="d-flex text-body-secondary pt-3">
+            <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
+                xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
+                preserveAspectRatio="xMidYMid slice" focusable="false">
+                <title>comentário</title>
+                <rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff"
+                    dy=".3em">32x32</text>
+            </svg>
+            <p class="pb-3 mb-0 small lh-sm border-bottom">
+                <strong class="d-block text-gray-dark">@${item.author}</strong>
+                ${item.comment}
+            </p>
+        </div>
+        
         `
-        body.appendChild(divDisplay);
+        divFeed.appendChild(divDisplay);
     })
-
-
 }
