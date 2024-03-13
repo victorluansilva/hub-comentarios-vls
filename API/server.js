6e13035233c09627aeb6ab853e2e05372a236902
victorluansilva <victorluansilva@gmail.com>
2024-03-13 19:01:54 -0300

    Aula dia 12-03 criação da API e do BD

--- API/server.js
+++ API/server.js
@@ -14,6 +14,14 @@ server.use(bodyParser.json());
 
 const PORT = 7000;
 
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
+
 server.listen(PORT, () =>{
     console.log(`O server está rodando em http:\\localhost:${PORT}`)
 } )
