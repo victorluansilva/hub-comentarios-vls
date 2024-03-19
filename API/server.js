8d8744f51781ab66709f3cfd537a4b345c33fdd7
victorluansilva <victorluansilva@gmail.com>
2024-03-19 18:58:07 -0300

    Criando login e endpoints necessários na API

--- API/server.js
+++ API/server.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 const server = express();
 
 server.use(express.json());
-server.use(express.urlencoded({extended: true}));
+server.use(express.urlencoded({ extended: true }));
 server.use(cors());
 server.use(bodyParser.json());
 
@@ -25,6 +25,23 @@ const db = mysql.createConnection({
 db.connect((err) => {
     if (err) return console.log(err);
     console.log('Conectado com sucesso!');
+})
+
+server.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    db.query('SELECT * FROM user WHERE username = ? AND password = ?',
+     [username, password], (err, results) => {
+        if (err) {
+            res.status(500).json({ success: false, error: 'Internal server error' });
+            return;
+        }
+        if (results.length > 0) {
+            const { id, username, firstname, lastname } = results[0];
+            res.json({ success: true, user: { id, username, firstname, lastname } });
+        } else {
+            res.json({ success: false, error: 'Usuário ou senha inválidos' });
+        }
+    })
 })
 
 server.get('/comment', (req, res) => {
@@ -45,6 +62,6 @@ server.get('/user', (req, res) => {
     });
 });
 
-server.listen(PORT, () =>{
+server.listen(PORT, () => {
     console.log(`O server está rodando em http:\\localhost:${PORT}`)
-} )
+})
