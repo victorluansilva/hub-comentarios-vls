336eefdc42e3aa2ef8176b98ea37d01147a7b571
victorluansilva <victorluansilva@gmail.com>
2024-04-01 20:03:11 -0300

    Adaptação do model de comment

--- FRONTEND/models/comment.model.js
+++ FRONTEND/models/comment.model.js
@@ -1,14 +1,23 @@
 class Comment {
 
-    constructor(id, author, comment_text, created_at, updated_at) {
-        if (id !== undefined, author !== undefined, comment_text !== undefined, created_at !== undefined, updated_at !== undefined) {
+    constructor(id, userId, author, comment_text, created_at, updated_at) {
+        if (id !== undefined, userId !== undefined, author !== undefined, comment_text !== undefined, created_at !== undefined, updated_at !== undefined) {
             this.id = id;
+            this.userId = userId;
             this.author = author;
             this.comment_text = comment_text
             this.created_at = created_at;
             this.updated_at = updated_at;
+        } else if (userId !== undefined, comment_text !== undefined) {
+            this.id = null;
+            this.userId = userId;
+            this.author = null;
+            this.comment_text = comment_text
+            this.created_at = null;
+            this.updated_at = null;
         } else if (author !== undefined, comment_text !== undefined) {
             this.id = null;
+            this.userId = null;
             this.author = author;
             this.comment_text = comment_text
             this.created_at = null;
